c2f2c2d986596f25fe10e226e70915f7b35ddfce
Jeffrey currie <taycurrie@gmail.com>
2020-07-27 21:03:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,32 +1,3 @@
-class Dog 
-  attr_accessor :name 
-  
-  @@all = []
-  
-   def initialize(name)
-    @name = name 
-    @@all << self
-     save
-   end
- 
-  def self.all
-    @@all
-  end
-  
-  def  self.print_all
-    puts @@all.map{ |dog| dog.name}
-  end
- 
-  def self.clear_all
-   @@all.clear
-  end
-  
-  def save
-    @@all << self
-  end
-  
 
-end
-  
 
   
